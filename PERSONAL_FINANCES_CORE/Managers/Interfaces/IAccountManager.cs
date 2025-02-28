21d74d1bdd0215143537fd04b570aced5372f4c0
Jazmin Esparza <jazminesp71@gmail.com>
2025-02-28 10:32:41 -0600

    Clase terminada y comentada Interface Account Manager

--- PERSONAL_FINANCES_CORE/Managers/Interfaces/IAccountManager.cs
+++ PERSONAL_FINANCES_CORE/Managers/Interfaces/IAccountManager.cs
@@ -3,15 +3,26 @@ using PERSONAL_FINANCES_CORE.Enums;
 
 namespace PERSONAL_FINANCES_CORE.Managers.Interfaces;
 
-public class IAccountManager
+public interface IAccountManager
 {
-    account getaccount(int id_account);
+    //obtiene cuenta con id
+    Account getaccount(int id_account);
 
+    //creación de cuentas nuevas con sus atributos
     bool newAccount(string owner, double money, double goal, double budget, double dategoal);
     
+    //obtener el saldo actual
     double currentBalance(int id_account);
     
+    //obtienen información de la cuenta
     string report(int id_account);
     
-    bool updateBalance(int id_account, TransactionManager transactionManager);
+    //Actualiza el saldo 
+    bool updateBalance(int id_account, double amount);
+    
+    //transacciones y estado de la cuenta 
+    string summary (int id_account, TransactionManager transactionManager);
+    
+    //Obtiene cuentas
+    List<Account> listAccounts();
 }
